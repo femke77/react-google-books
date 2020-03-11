b427a4a46a250b24dcb8cd8bfe812272778af619
Meg Meyers <megan.g.meyers@gmail.com>
2020-03-10 20:07:00 -0700

    backend structure with schema, front end structure, installs

---
+++ routes/api/book.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+//const booksController = require("../../controllers/booksController");
+
+router.get("/books", (req, res) => {
+
+});
+
+router.post("/books", (req, res) => {
+
+});
+
+router.get("/books/:id", (req, res) => {
+
+});
